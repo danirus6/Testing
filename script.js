688c8d82d3372d86c0dc3e4d218293d5045a94ac
danirus6 <44194174+danirus6@users.noreply.github.com>
2023-11-17 19:05:06 +0100

    Update

--- script.js
+++ script.js
@@ -1,79 +1,38 @@
-// Función principal
-function sumArray(array) {
-    // Paso 1: Verifica si el array cumple con las condiciones dadas
-    if (!array || !Array.isArray(array) || array.length <= 2) {
-        return 0; // Devuelve 0 si el array no cumple con las condiciones
-    }
-
-    // Inicializa variables para el valor mínimo, máximo y la suma
-    let min = array[0];
-    let max = array[0];
-    let sum = 0;
-
-    // Paso 1: Recorre el array para calcular la suma, el valor mínimo y el valor máximo
-    for (let i = 0; i < array.length; i++) {
-        sum += array[i];
-
-        // Actualiza el valor mínimo si se encuentra un valor menor
-        if (array[i] < min) {
-            min = array[i];
-        }
-
-        // Actualiza el valor máximo si se encuentra un valor mayor
-        if (array[i] > max) {
-            max = array[i];
-        }
-    }
-
-    // Devuelve la suma excluyendo el valor mínimo y máximo
-    return sum - min - max;
-}
-
-// Función alternativa con sort y reduce
 function sumArrayWithSort(array) {
-    // Paso 1: Verifica si el array cumple con las condiciones dadas
     if (!array || !Array.isArray(array) || array.length <= 2) {
-        return 0; // Devuelve 0 si el array no cumple con las condiciones
+        return 0; 
     }
 
-    // Paso 2: Ordena el array de forma ascendente
     array.sort(function (a, b) {
         return a - b;
     });
 
-    // Paso 3: Utiliza slice y reduce para calcular la suma excluyendo el primer y último elemento
+    
     return array.slice(1, -1).reduce(function (acc, val) {
         return acc + val;
     }, 0);
 }
 
-// Función alternativa sin sort ni reduce
 function sumArrayWithoutSortReduce(array) {
-    // Paso 1: Verifica si el array cumple con las condiciones dadas
     if (!array || !Array.isArray(array) || array.length <= 2) {
-        return 0; // Devuelve 0 si el array no cumple con las condiciones
+        return 0; 
     }
 
-    // Inicializa variables para el valor mínimo, máximo y la suma
     let min = array[0];
     let max = array[0];
     let sum = 0;
 
-    // Paso 2: Recorre el array para calcular la suma, el valor mínimo y el valor máximo
     for (let i = 0; i < array.length; i++) {
         sum += array[i];
 
-        // Actualiza el valor mínimo si se encuentra un valor menor
         if (array[i] < min) {
             min = array[i];
         }
 
-        // Actualiza el valor máximo si se encuentra un valor mayor
         if (array[i] > max) {
             max = array[i];
         }
     }
 
-    // Devuelve la suma excluyendo el valor mínimo y máximo
     return sum - min - max;
 }
--- tests/spec.js
+++ tests/spec.js
@@ -1,55 +1,39 @@
 describe('sumArray', function () {
-    // Describe un conjunto de pruebas para la función sumArray.
     it('should return 0 for null', function () {
-        // Prueba: Debería devolver 0 para un valor nulo.
-        expect(sumArray(null)).toEqual(0);
+        
         expect(sumArrayWithSort(null)).toEqual(0);
         expect(sumArrayWithoutSortReduce(null)).toEqual(0);
-        // Se prueba la función principal y las funciones alternativas con un valor nulo.
     });
 
     it('should return 0 for an empty array', function () {
-        // Prueba: Debería devolver 0 para un array vacío.
-        expect(sumArray([])).toEqual(0);
+        
         expect(sumArrayWithSort([])).toEqual(0);
         expect(sumArrayWithoutSortReduce([])).toEqual(0);
-        // Se prueba la función principal y las funciones alternativas con un array vacío.
     });
 
     it('should return 0 for an array with one element', function () {
-        // Prueba: Debería devolver 0 para un array con un solo elemento.
-        expect(sumArray([3])).toEqual(0);
+        
         expect(sumArrayWithSort([3])).toEqual(0);
         expect(sumArrayWithoutSortReduce([3])).toEqual(0);
-        // Se prueba la función principal y las funciones alternativas con un array que tiene un solo elemento.
     });
 
     it('should return 0 for an array with two elements', function () {
-        // Prueba: Debería devolver 0 para un array con dos elementos.
-        expect(sumArray([3, 5])).toEqual(0);
+        
         expect(sumArrayWithSort([3, 5])).toEqual(0);
         expect(sumArrayWithoutSortReduce([3, 5])).toEqual(0);
-        // Se prueba la función principal y las funciones alternativas con un array que tiene dos elementos.
     });
 
     it('should return the correct sum for an array with more than two elements', function () {
-        // Prueba: Debería devolver la suma correcta para un array con más de dos elementos.
-        expect(sumArray([6, 2, 1, 8, 10])).toEqual(16);
-        expect(sumArray([0, 1, 6, 10, 10])).toEqual(17);
-        expect(sumArray([-6, -20, -1, -10, -12])).toEqual(-28);
-        expect(sumArray([-6, 20, -1, 10, -12])).toEqual(3);
-        // Se prueba la función principal con varios arrays y se comparan con los resultados esperados.
+    
 
         expect(sumArrayWithSort([6, 2, 1, 8, 10])).toEqual(16);
         expect(sumArrayWithSort([0, 1, 6, 10, 10])).toEqual(17);
         expect(sumArrayWithSort([-6, -20, -1, -10, -12])).toEqual(-28);
         expect(sumArrayWithSort([-6, 20, -1, 10, -12])).toEqual(3);
-        // Se prueba la función alternativa con sort y reduce con los mismos arrays.
 
         expect(sumArrayWithoutSortReduce([6, 2, 1, 8, 10])).toEqual(16);
         expect(sumArrayWithoutSortReduce([0, 1, 6, 10, 10])).toEqual(17);
         expect(sumArrayWithoutSortReduce([-6, -20, -1, -10, -12])).toEqual(-28);
         expect(sumArrayWithoutSortReduce([-6, 20, -1, 10, -12])).toEqual(3);
-        // Se prueba la función alternativa sin sort ni reduce con los mismos arrays.
     });
 });
